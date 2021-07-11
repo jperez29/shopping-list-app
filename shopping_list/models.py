482a68e44dd1c8d7a1e6e39618c98c723d85e1bc
Janet <perezjanet103@gmail.com>
2021-07-11 18:20:10 -0400

    editing files

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -1,22 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-# import uuid
 
-#creating a users model (one to many relationship)
-# class User(models.Model):
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     email = models.EmailField(primary_key=True)
-
-#     def __str__(self):
-#         return f"<{self.first_name} {self.last_name}>"
-    # email = models.CharField(primary_key = True, max_length=30)
-#     #UUIDField is a field for storing universally unique identifiers
-#     id = models.UUIDField(primary_key = True, default = uuid.uuid4,editable = False)
 
 class ItemList(models.Model):
     item = models.TextField()
-    # id = models.UUIDField(primary_key = True, default = uuid.uuid4,editable = False)
     user = models.ForeignKey(User, null = True, on_delete = models.CASCADE)
     
     def __str__(self):
